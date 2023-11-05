38dea7a860bd78668023de5681fb376228cc0673
KeveXD <100792669+KeveXD@users.noreply.github.com>
2023-11-05 01:15:00 +0100

    kakas

--- develop3/buxa/lib/model/new_person_model.dart
+++ develop3/buxa/lib/model/new_person_model.dart
@@ -1,10 +1,37 @@
 import 'package:buxa/database/person_repository.dart';
 import 'package:buxa/data_model/person_data_model.dart';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 
 class NewPersonModel {
   Future<int?> insertPerson(String name, String email, bool hasRevolut) async {
     if (kIsWeb) {
+      // Webes platformon Firestore használata
+      try {
+        final user = FirebaseAuth.instance.currentUser;
+        if (user != null) {
+          final firestore = FirebaseFirestore.instance;
+          final userEmail = user.email;
+
+          final peopleCollectionRef = firestore
+              .collection(userEmail!)
+              .doc('userData')
+              .collection('People');
+          final newPerson = PersonDataModel(
+            name: name,
+            email: email,
+            hasRevolut: hasRevolut,
+          );
+
+          final docRef = await peopleCollectionRef.add(newPerson.toMap());
+          return newPerson.id;
+        }
+      } catch (error) {
+        print('Hiba történt: $error');
+        return null;
+      }
     } else {
       // Mobil platformon használjuk a PersonRepository-t
       final dbHelper = PersonRepository();
