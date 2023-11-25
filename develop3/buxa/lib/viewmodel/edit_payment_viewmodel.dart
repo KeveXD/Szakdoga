9086875584dd60654e7e95b5a051a4e1775c0cb6
KeveXD <100792669+KeveXD@users.noreply.github.com>
2023-11-25 19:03:47 +0100

    humm

--- develop3/buxa/lib/viewmodel/edit_payment_viewmodel.dart
+++ develop3/buxa/lib/viewmodel/edit_payment_viewmodel.dart
@@ -6,8 +6,9 @@ import 'package:flutter/foundation.dart';
 
 class EditPaymentViewModel {
   Future<int> updatePayment(PaymentDataModel updatedPayment) async {
-    print("lol");
+    print("ez: ${updatedPayment.id}");
     if (kIsWeb) {
+      deletePaymentWeb(updatedPayment.title ?? "");
       try {
         final firestore = FirebaseFirestore.instance;
         final user = FirebaseAuth.instance.currentUser;
@@ -46,6 +47,41 @@ class EditPaymentViewModel {
       }
     } else {
       return await PaymentRepository().updatePayment(updatedPayment);
+    }
+
+    return 0;
+  }
+
+  Future<int> deletePaymentWeb(String name) async {
+    if (kIsWeb) {
+      print("delete: $name");
+      try {
+        final firestore = FirebaseFirestore.instance;
+        final user = FirebaseAuth.instance.currentUser;
+        String? userEmail;
+
+        if (user != null) {
+          userEmail = user.email;
+          final paymentCollectionRef = firestore
+              .collection(userEmail!)
+              .doc('userData')
+              .collection('Payment');
+
+          final personQuerySnapshot =
+              await paymentCollectionRef.where('title', isEqualTo: name).get();
+          if (personQuerySnapshot.docs.isNotEmpty) {
+            // Csak egy elemet töröljünk, ha több azonosító is megegyezik, válasszuk ki azt, amelyiket törölni szeretnénk
+            final personDocRef = personQuerySnapshot.docs.first.reference;
+            await personDocRef.delete();
+          } else {}
+        } else {
+          print('Nem vagy bejelentkezve.');
+        }
+      } catch (e) {
+        print('Hiba történt a személy törlése közben: $e');
+      }
+    } else {
+      return 0;
     }
 
     return 0;
